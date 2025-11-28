dc1183cc4188d80aa1c78b61c07a7177af76943f
a.nikitin52 <a.nikitin0552@gmail.com>
2025-11-28 08:48:24 +0300

    fixed sortings

--- src/sortings.cpp
+++ src/sortings.cpp
@@ -1,67 +1,30 @@
 #include "AVLtree.h"
 
 void divide(int a[], int i, int j, int& i1) {
-    int k = (i + j) / 2;
+    int k = (i + j) / 2;  // СРЕДНИЙ элемент, а не остаток от деления!
     int b = a[k];
     i1 = i;
     int j1 = j;
 
-    // Переносим опорный элемент в начало
-    int temp = a[k];
-    a[k] = a[i];
-    a[i] = temp;
-
-    int pivot = a[i]; // опорный элемент теперь в позиции i
-    i1 = i;
-    j1 = j;
-
-    while (i1 <= j1) {
-        while (i1 <= j1 && a[i1] <= pivot) i1++;
-        while (i1 <= j1 && a[j1] >= pivot) j1--;
-        if (i1 < j1) {
-            temp = a[i1];
+    do {
+        while (a[j1] > b && j1 > i) j1--;
+        while (a[i1] < b && i1 < j) i1++;
+        if (i1 <= j1) {
+            int temp = a[i1];
             a[i1] = a[j1];
             a[j1] = temp;
+            i1++;
+            j1--;
         }
-    }
-
-    // Возвращаем опорный элемент на правильную позицию
-    temp = a[i];
-    a[i] = a[j1];
-    a[j1] = temp;
-
-    i1 = j1; // i1 теперь указывает на позицию опорного элемента
+    } while (i1 <= j1);
 }
 
 void quickSort(int a[], int i, int j) {
-    // Итеративная версия с явным стеком
-    const int MAX_STACK_SIZE = 1000;
-    int stack[MAX_STACK_SIZE];
-    int top = -1;
-
-    stack[++top] = i;
-    stack[++top] = j;
-
-    while (top >= 0) {
-        j = stack[top--];
-        i = stack[top--];
-
-        if (i < j) {
-            int i1;
-            divide(a, i, j, i1);
-
-            // Добавляем в стек правую часть
-            if (i1 + 1 < j) {
-                stack[++top] = i1 + 1;
-                stack[++top] = j;
-            }
-
-            // Добавляем в стек левую часть  
-            if (i < i1 - 1) {
-                stack[++top] = i;
-                stack[++top] = i1 - 1;
-            }
-        }
+    if (i < j) {
+        int i1;
+        divide(a, i, j, i1);
+        quickSort(a, i, i1 - 1);
+        quickSort(a, i1, j);  
     }
 }
 
